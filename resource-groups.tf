7f01a79a2712d58b94b3b2ce1193c37becd219e2
Gordon Beeming <gordon@devstarops.com>
2022-08-03 21:50:42 +0200

    reference existing resource group

--- resource-groups.tf
+++ resource-groups.tf
@@ -1,13 +1,8 @@
 
-resource "azurerm_resource_group" "demo" {
+data "azurerm_resource_group" "demo" {
   name     = var.resource_group_name
-  location = var.deploy_region
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 output "demo_resource_group" {
-  value = azurerm_resource_group.demo.name
+  value = data.azurerm_resource_group.demo.name
 }
